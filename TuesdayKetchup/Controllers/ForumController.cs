629a2112d4208ba26f9384338078f7eaa78d108f
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-08-06 15:16:05 -0500

    Created the basis for the forum pages

---
+++ TuesdayKetchup/Controllers/ForumController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TuesdayKetchup.Models;
+
+namespace TuesdayKetchup.Controllers
+{
+    public class ForumController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Forum
+        public ActionResult Index()
+        {
+            return View(db.threads.ToList());
+        }
+
+        // GET: Forum/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Thread thread = db.threads.Find(id);
+            if (thread == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thread);
+        }
+
+        // GET: Forum/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Forum/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title")] Thread thread)
+        {
+            if (ModelState.IsValid)
+            {
+                db.threads.Add(thread);
+                db.SaveChanges();
+                return RedirectToAction("CreatePost");
+            }
+
+            return View(thread);
+        }
+
+        public ActionResult CreatePost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreatePost([Bind(Include = "Id,UserId,ThreadId,Message")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        // GET: Forum/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Thread thread = db.threads.Find(id);
+            if (thread == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thread);
+        }
+
+        // POST: Forum/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title")] Thread thread)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thread).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(thread);
+        }
+
+        // GET: Forum/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Thread thread = db.threads.Find(id);
+            if (thread == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thread);
+        }
+
+        // POST: Forum/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Thread thread = db.threads.Find(id);
+            db.threads.Remove(thread);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
